e06d478bf60667955d6800464e2ec2ad7e095f45
makcmakc <makcmakc321@mail.ru>
2020-01-22 11:51:22 +0300

    commit

---
+++ models/comment.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema(
+  {
+    body: {
+      type: String,
+      required: true
+    },
+    post: {
+      type: Schema.Types.ObjectId,
+      ref: 'Post'
+    },   
+    parent: {
+      type: Schema.Types.ObjectId,
+      ref: 'Comment'
+    },      
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    children: {
+      type: Schema.Types.ObjectId,
+      ref: 'Comment'
+    },    
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  {
+    timestamps: false
+  }
+);
+
+schema.set('toJSON', {
+  virtuals: true
+});
+
+module.exports = mongoose.model('Comment', schema);
